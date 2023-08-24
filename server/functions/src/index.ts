783394df1101f72db6f49ea82efd84dd576b2e11
Ahmet <exarons@gmail.com>
2023-08-24 22:05:10 +0300

    Cloud functions

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,20 +1,54 @@
-/* eslint-disable object-curly-spacing */
 import express from "express";
 import cors from "cors";
 import { getFirestore } from "firebase-admin/firestore";
 import { onRequest } from "firebase-functions/v1/https";
-import { initializeApp } from "firebase-admin";
+import { initializeApp } from "firebase-admin/app";
 
 const app = express();
-
-initializeApp();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+initializeApp();
+
+const db = getFirestore();
+
+//User register route
 app.post("/register", async (req, res) => {
-  const { UID, email, username, password } = req.body;
+  let { UID, email, username, password, platform, gender } = req.body;
+
+  if (!gender) {
+    gender = "Unknown"
+  }
+
+  //Getting user document that we will use to store user data
+  const userRef = db.collection("Users").doc(UID);
+
+  //Register with Google
+  if (platform === "GOOGLE") {
+    try {
+      await userRef.set({
+        email,
+        username,
+      });
+
+      res.status(200).send({ message: "User registered successfully" });
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong" });
+    }
+  }
+  //Register with form
+  else {
+    await userRef.set({
+      email,
+      username,
+      password,
+      gender
+    });
+
+    res.status(200).send({ message: "User registered successfully" });
+  }
 });
 
 exports.app = onRequest(app);
